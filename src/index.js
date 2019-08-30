1a44ae99d789e0e9de50afc7b678155bfd156e67
Nerldy_ <arnodhis@gmail.com>
2019-08-30 12:41:03 +0300

    todo list

--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
 import React, { Component, useState } from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
+import uuid from "uuid/v1";
 import "./index.css";
 
-const App = () => {
-	const [theme, setTheme] = useState("grey");
+/*
+  INSTRUCTIONS:
+  Create a "todo" app with the following criteria.
+    1. The user can add new todo items
+    2. The user can remove todo items
+*/
 
-	const toBlue = () => setTheme("blue");
-	const toGrey = () => setTheme("grey");
+const Todo = () => {
+	const [userInput, setUserInput] = useState(null);
+	const [todos, setTodo] = useState([]);
+
+	const readInputData = e => {
+		setUserInput(e.target.value);
+	};
+	const createTodo = () => {
+		setTodo([...todos, { id: uuid(), todo: userInput }]);
+	};
+
+	const removeTodoItem = id => {
+		const delTodo = todos.filter(todo => todo.id !== id);
+		setTodo([...delTodo]);
+	};
 
 	return (
-		<div className={`boxer ${theme}`}>
-			{theme === "grey" ? (
-				<button onClick={toBlue}>Blue</button>
-			) : (
-				<button onClick={toGrey}>Grey</button>
-			)}
+		<div>
+			<input type="text" onChange={readInputData} />
+			<button onClick={createTodo}>Add Todo</button>
+			<ul>
+				{todos.map(todo => (
+					<li key={todo.id}>
+						{todo.todo}{" "}
+						<button onClick={() => removeTodoItem(todo.id)}>
+							Delete
+						</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Todo />, document.getElementById("root"));
