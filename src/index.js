2093008b9a1c2e7f9cabbfb6df5ca7fcc22148f7
Nerldy_ <arnodhis@gmail.com>
2019-09-04 10:39:25 +0300

    to be cont

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import "./index.css";
 
+/*
+  Instructions:
+    Assume you're creating an app that allows the user to 
+    post status updates (ala Twitter). Your UI should have a
+    textarea and a button. The button should be disabled if the
+    length of the textarea is 0 or greater than 240 characters.
+    The document's title should inform the user on how many
+    characters they have left to type before they hit the 240
+    character limit - "115 characters left."
+*/
+
 const App = () => {
-	const [theme, setTheme] = useState("grey");
+	const [maxLen, calculateRemainingLen] = useState(240);
+	const [statusValue, setStatusValue] = useState("");
+	// const [textLen, setTextLen ] = useEffect(maxLen)
 
-	const toBlue = () => setTheme("blue");
-	const toGrey = () => setTheme("grey");
-
+	const checkStatusValue = e => {
+		const textLen = e.target.value.length;
+		console.log({ textLen });
+		setStatusValue(e.target.value);
+	};
+	useEffect(() => {
+		console.log("render");
+	}, [maxLen, textLen]);
 	return (
-		<div className={`boxer ${theme}`}>
-			{theme === "grey" ? (
-				<button onClick={toBlue}>Blue</button>
-			) : (
-				<button onClick={toGrey}>Grey</button>
-			)}
+		<div>
+			<p>{maxLen} characters left</p>
+			<textarea
+				name="status"
+				id="status"
+				cols="30"
+				rows="10"
+				required
+				maxLength="240"
+				value={statusValue}
+				onChange={checkStatusValue}></textarea>
 		</div>
 	);
 };
